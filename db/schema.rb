a702a3fe1a00739a12c5b3304e4e985c64833c04
Rose Connolly <Rose@Roses-MacBook-Air.local>
2014-05-28 12:00:18 -0400

    added status to notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140528145844) do
+ActiveRecord::Schema.define(version: 20140528155906) do
 
   create_table "leads", force: true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140528145844) do
     t.text     "notes_body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "status"
   end
 
   create_table "users", force: true do |t|
