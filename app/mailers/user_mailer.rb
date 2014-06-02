d0c1f6dfd8b1e6a894c0b8dbdb62651183a6bbc1
Rose Connolly <Rose@Roses-MacBook-Air.local>
2014-06-02 10:28:15 -0400

    email issues

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,16 @@
 class UserMailer < ActionMailer::Base
   default from: 'support@weand.co'
  
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://weand.co'
+    mail(to: @user.email, subject: 'Thank you for contacting us!')
+  end
+
   def notice_email(admin)
     @admin = admin
     @url  = 'http://weand.co'
     mail(to: @self.email, subject: 'A new lead has contacted us!')
   end
+
 end
